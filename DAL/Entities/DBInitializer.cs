554c483616e30af2fd8c5542fdd7759ac7333386
novak vova <novakvova@gmail.com>
2017-10-08 09:59:57 +0300

    Begin commit DAL work

---
+++ DAL/Entities/DBInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace DAL.Entities
+{
+    public class DBInitializer : CreateDatabaseIfNotExists<EFContext> //DropCreateDatabaseAlways<EFContext> //
+    {
+        protected override void Seed(EFContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
---
+++ DAL/Entities/EFContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("ShopConnection")
+        {
+            Database.SetInitializer<EFContext>(null);
+        }
+        public EFContext(string connString)
+            : base(connString)
+        {
+            Database.SetInitializer<EFContext>(new DBInitializer());
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
